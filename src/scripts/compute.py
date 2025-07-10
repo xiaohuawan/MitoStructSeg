e99759ec4aeb68825e375d33c1eec1f28fa21216
xiaohuawan <148045263+xiaohuawan@users.noreply.github.com>
2025-07-10 20:49:38 +0800

    Update

--- src/scripts/compute.py
+++ src/scripts/compute.py
@@ -20,7 +20,7 @@ class MitoCompute:
         os.chdir(script_dir)
         self.mito_segment_path = os.path.join(script_dir)
 
-        # 创建用于保存图像的png目录
+
         self.save_image_dir = os.path.join(self.mito_segment_path, 'local_outputs')
         if os.path.exists(self.save_image_dir):
             shutil.rmtree(self.save_image_dir)
@@ -28,7 +28,7 @@ class MitoCompute:
         # if not os.path.exists(self.save_image_dir):
         #     os.makedirs(self.save_image_dir)
 
-        # 本地目录保存中间文件
+
         self.local_save_dir = os.path.join(self.mito_segment_path, 'local_outputs')
         # if not os.path.exists(self.local_save_dir):
         #     os.makedirs(self.local_save_dir)
@@ -68,9 +68,7 @@ class MitoCompute:
         self.check_image_paths(self.imgList)
 
     def is_url(self, path):
-        """
-        检查路径是否为URL
-        """
+
         parsed = urlparse(path)
         return parsed.scheme in ('http', 'https')
 
@@ -94,14 +92,12 @@ class MitoCompute:
         print("Validated image list: ", self.imgList)
 
     def handle(self):
-        """
-        处理图像，存储计算结果到本地，然后复制到目标目录
-        """
+
         self.getContours()
         len_out, len_inside, single_len_out, single_len_inside = self.calculate(self.labeled_array, self.images_labeled, self.cntLabel)
         self.filtered(self.cntLabel, len_out, len_inside, single_len_out, single_len_inside)
 
-        # 存储结果到本地
+
         pix_length = 5
         pix_height = 10
         self.filtered_len_out = [i * pix_length * pix_height for i in self.filtered_len_out]
@@ -112,7 +108,7 @@ class MitoCompute:
             "total_inside": self.filtered_len_inside
         })
 
-        # 保存每个图像的线粒体的计算结果
+
         single_cols = ["image_name"]
         for i in range(self.cntLabel_filtered):
             single_cols += [f"label_{i + 1}_extral", f"label_{i + 1}_insider"]
@@ -122,7 +118,7 @@ class MitoCompute:
         total_df.to_csv(os.path.join(self.local_save_dir, "total_info.csv"), index=False)
         logger.info("total_df saved locally!")
 
-        # 保存处理后的图像到png目录中，并生成seg.json文件
+
         image_urls = []
         for img_index, img_name in enumerate(self.all_imageName):
             filename_without_extension = os.path.splitext(img_name)[0]
@@ -130,34 +126,32 @@ class MitoCompute:
             filepath = os.path.join(self.lable_dir, filename)
             np.save(filepath, arr=self.filtered_labeled_array[img_index])
 
-            # 保存每张处理后的图像到png目录中
+
             img_save_path = os.path.join(self.save_image_dir, f"{filename_without_extension}.png")
             self.filtered_images_labeled.append(cv2.imread(img_save_path))
             cv2.imwrite(img_save_path, self.filtered_images_labeled[img_index])
             
-            # 生成对应的 URL
+
             image_url = f"http://localhost:3000/free/outputs/{filename_without_extension}.png"
             image_urls.append(image_url)
 
-        # 保存seg.json
+
         with open(os.path.join(self.save_image_dir, 'seg.json'), 'w') as f:
             json.dump(image_urls, f, indent=4)
         
         logger.info("seg.json saved locally!")
 
-        # 复制文件到目标目录
+
         self.copy_to_local_directory("single_info.csv")
         self.copy_to_local_directory("total_info.csv")
         for file_name in os.listdir(self.lable_dir):
             self.copy_to_local_directory(file_name)
-        # 如果需要复制保存的图像
+
         for file_name in os.listdir(self.save_image_dir):
             self.copy_to_local_directory(file_name)
 
     def copy_to_local_directory(self, file_name):
-        """
-        复制文件到指定的本地目录
-        """
+
         source_file_path = os.path.join(self.local_save_dir, file_name)
         # target_directory = r"C:\Users\39767\Desktop\app1\berry-free-react-admin-template-main8.23\vite\public\outputs"
         target_directory = os.path.join(os.path.abspath('../../vite/public/outputs'))
@@ -174,10 +168,7 @@ class MitoCompute:
             logger.error(f"Exception occurred while copying {file_name}: {str(e)}")
 
     def getContours(self):
-        """
-        获取连通域数量，给每张图片打上连通域序号标签
-        """
-        # 获取图片
+
         for imgindex, imgpath in enumerate(self.imgList):
             imgname = imgpath.split(os.sep)[-1]
             self.all_imageName.append(imgname)
@@ -192,7 +183,7 @@ class MitoCompute:
         
         self.img_shape = self.images[0].shape
         
-        # 获取外侧轮廓并填充，寻找连通域
+
         imgs_fill = []
         for img in self.images:
             filled_image = np.copy(img)
@@ -203,9 +194,9 @@ class MitoCompute:
 
         stacked_image_fill = np.stack(imgs_fill)
         self.labeled_array, self.cntLabel = ndimage.label(stacked_image_fill)
-        logger.info(f"所有连通域数量为{self.cntLabel}")
+        logger.info(f"{self.cntLabel}")
 
-        # 设置标签对应图片列表
+
         for index, labeled in enumerate(self.labeled_array):
             img_labeled = np.zeros(self.img_shape, dtype=np.uint8)
             img_labeled[labeled != 0] = 255
@@ -213,9 +204,7 @@ class MitoCompute:
             self.images_labeled.append(img_labeled)
 
     def calculate(self, labeled_array, images_labeled, cntLabel):
-        """
-        计算膜长度信息
-        """
+
         len_out = np.zeros(cntLabel + 1)
         len_inside = np.zeros(cntLabel + 1)
         single_len_out = np.zeros((len(self.imgList), cntLabel + 1))
@@ -250,9 +239,7 @@ class MitoCompute:
         return len_out, len_inside, single_len_out, single_len_inside
 
     def filtered(self, cntLabel, len_out, len_inside, single_len_out, single_len_inside):
-        """
-        去除内膜为0的部分，并在图像上标注每个线粒体的标签并绘制外膜颜色
-        """
+
         filtered_labels = []
         for i in range(1, cntLabel + 1):
             if len_inside[i] != 0 and len_out[i] != 0:
@@ -260,15 +247,15 @@ class MitoCompute:
                 self.filtered_len_out.append(len_out[i])
                 self.filtered_len_inside.append(len_inside[i])
         self.cntLabel_filtered = len(filtered_labels)
-        logger.info(f"剔除内膜为0的连通域后，连通域数量为{self.cntLabel_filtered}")
+        logger.info(f"{self.cntLabel_filtered}")
 
-        # 参考映射 filter_refer[a]=b 表示原始标签为a的现在标签值是b
+     
         filter_refer = np.zeros(cntLabel + 1)
         for index, label in enumerate(filtered_labels):
             filter_refer[label] = int(index) + 1
         filter_refer = filter_refer.astype(int)
 
-        # 修改新的标签值
+
         self.filtered_labeled_array = np.copy(self.labeled_array)
         with tqdm(total=self.filtered_labeled_array.shape[0], desc="Processing", unit="images") as pbar:
             for index, img in enumerate(self.filtered_labeled_array):
@@ -276,26 +263,25 @@ class MitoCompute:
                 img[:] = np.vectorize(lambda x: filter_refer[x])(img)
                 pbar.update()
 
-        # 标签图片相对应，并标注标签
         for index, labeled in enumerate(self.filtered_labeled_array):
-            # 创建彩色图像用于绘制轮廓和标注
+
             img_labeled_color = cv2.cvtColor(self.images[index], cv2.COLOR_GRAY2BGR)
 
-            # 生成每个线粒体的随机颜色
-            np.random.seed(42)  # 固定随机种子，确保每次生成的颜色相同
+
+            np.random.seed(42) 
             colors = {label: np.random.randint(0, 255, 3).tolist() for label in np.unique(labeled) if label != 0}
 
-            # 获取每个线粒体的中心位置，并标注标签
+
             unique_labels = np.unique(labeled)
             for label in unique_labels:
                 if label == 0:
-                    continue  # 跳过背景标签
+                    continue
 
-                # 创建mask并计算中心
+
                 mask = np.zeros_like(labeled, dtype=np.uint8)
                 mask[labeled == label] = 255
 
-                # 找到外膜轮廓并绘制颜色
+ 
                 contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                 cv2.drawContours(img_labeled_color, contours, -1, colors[label], 2)
 
@@ -304,17 +290,17 @@ class MitoCompute:
                     cX = int(moments["m10"] / moments["m00"])
                     cY = int(moments["m01"] / moments["m00"])
 
-                    # 在彩色图像上绘制红色标签
+
                     label_text = f"{label}"
                     cv2.putText(img_labeled_color, label_text, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-            # 保存每张带编号的图像, 使用原始文件名
+
             filename_without_extension = os.path.splitext(self.all_imageName[index])[0]
             img_save_path = os.path.join(self.save_image_dir, f"{filename_without_extension}.png")
             self.filtered_images_labeled.append(img_labeled_color)
             cv2.imwrite(img_save_path, img_labeled_color)
 
-        # 处理单个线粒体计算结果
+
         filtered_single_len_out = single_len_out[:, filtered_labels]
         filtered_single_len_inside = single_len_inside[:, filtered_labels]
 
